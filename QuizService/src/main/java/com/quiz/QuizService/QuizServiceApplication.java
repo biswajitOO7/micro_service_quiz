c753cbf1995b43cf0d4067606662efe88e434d8d
bmajumdar <bmajumdar@xandr.com>
2024-04-18 03:55:38 +0530

    http communication between quiz and question using open feign

--- QuizService/src/main/java/com/quiz/QuizService/QuizServiceApplication.java
+++ QuizService/src/main/java/com/quiz/QuizService/QuizServiceApplication.java
@@ -1,9 +1,12 @@
 package com.quiz.QuizService;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients  // Enable all Feign clients is QuestionClient
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
