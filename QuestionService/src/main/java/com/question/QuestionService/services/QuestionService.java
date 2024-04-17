67f5b83c478d2986fb7908aba7ea0ed17393037b
bmajumdar <bmajumdar@xandr.com>
2024-04-17 11:26:46 +0530

    Geting all question of a quiz

--- QuestionService/src/main/java/com/question/QuestionService/services/QuestionService.java
+++ QuestionService/src/main/java/com/question/QuestionService/services/QuestionService.java
@@ -8,4 +8,5 @@ public interface QuestionService {
     Question create(Question question);
     List<Question> get();
     Question getOne(Long id);
+    List<Question> getQuestionsOfQuiz(Long quizId);  // gets all the question of a quiz
 }
